08e200f21868054112602ecb75114d5e71049039
YangCha <mbhband@naver.com>
2025-08-03 15:08:51 +0900

    Docker 제거

---
+++ my-dash-app/official_site/dash-layout/app.py
@@ -0,0 +1,57 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash()
+
+colors = {
+    "background": "#111111",
+    "text": "#7FDBFF",
+}
+
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
+
+fig = px.bar(
+    df,
+    x="Fruit",
+    y="Amount",
+    color="City",
+    barmode="group",
+)
+
+fig.update_layout(
+    plot_bgcolor=colors["background"],
+    paper_bgcolor=colors["background"],
+    font_color=colors["text"],
+)
+
+app.layout = html.Div(
+    children=[
+        html.H1(
+            children="Hello Dash",
+            style={
+                "textAlign": "center",
+                "color": colors["text"],
+            },
+        ),
+        html.Div(
+            children="""
+                 Dash: A Web application framework for your data
+                 """,
+            style={
+                "textAlign": "center",
+                "color": colors["text"],
+            },
+        ),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8050)
